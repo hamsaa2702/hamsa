911bb26d0852e46f30c3b5e8011ba6f54af34a99
Hamsa M <hamsaa2702@gmail.com>
2026-09-15 09:36:45 +0530

    Add files via upload

---
+++ 1.C
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push()
+{
+    int item;
+
+    if (top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        printf("Enter the element: ");
+        scanf("%d", &item);
+
+        top++;
+        stack[top] = item;
+
+        printf("%d element pushed successfully\n", item);
+    }
+}
+
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        printf("%d element popped successfully\n", stack[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Elements in the stack are:\n");
+
+        for (int i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                push();
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
