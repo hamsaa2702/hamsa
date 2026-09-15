e8d88249329e7b132932ae5bc21dda4db07fca94
Hamsa M <hamsaa2702@gmail.com>
2026-09-15 09:38:42 +0530

    Add files via upload

--- 1.C
+++ 1.C
@@ -1,93 +1,20 @@
-#include <stdio.h>
-
-#define MAX 5
-
-int stack[MAX];
-int top = -1;
-
-void push()
-{
-    int item;
-
-    if (top == MAX - 1)
-    {
-        printf("Stack Overflow\n");
-    }
-    else
-    {
-        printf("Enter the element: ");
-        scanf("%d", &item);
-
-        top++;
-        stack[top] = item;
-
-        printf("%d element pushed successfully\n", item);
-    }
-}
-
-void pop()
-{
-    if (top == -1)
-    {
-        printf("Stack Underflow\n");
-    }
-    else
-    {
-        printf("%d element popped successfully\n", stack[top]);
-        top--;
-    }
-}
-
-void display()
-{
-    if (top == -1)
-    {
-        printf("Stack is empty\n");
-    }
-    else
-    {
-        printf("Elements in the stack are:\n");
-
-        for (int i = top; i >= 0; i--)
-        {
-            printf("%d\n", stack[i]);
-        }
-    }
-}
-
+#include<stdio.h>
 int main()
 {
-    int choice;
-
-    while (1)
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);if(a>b)
     {
-        printf("\n1. Push\n");
-        printf("2. Pop\n");
-        printf("3. Display\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-            case 1:
-                push();
-                break;
-
-            case 2:
-                pop();
-                break;
-
-            case 3:
-                display();
-                break;
-
-            case 4:
-                return 0;
-
-            default:
-                printf("Invalid choice\n");
-        }
+        printf("%d is greater than %d",a,b);
     }
+    else if(b>a)
+    {
+        printf("%d is greater than %d",b,a);
+    }
+    else
+    {
+        printf("Both numbers are equal");
+    }
+
+    return 0;
 }
